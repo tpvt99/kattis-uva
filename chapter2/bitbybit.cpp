1ec238a0188d9cad5c4b822563e92c450083cc6f
Tran Phong <tran.bp@dyson.com>
2024-04-19 09:26:37 +0800

    update

--- chapter2/bitbybit.cpp
+++ chapter2/bitbybit.cpp
@@ -5,23 +5,25 @@
 
 int main() {
     int n;
+    int i = 0;
     while(scanf("%d\n", &n), n != 0) {
-        std::vector<char> vec{32, '?'};
+        std::vector<char> vec(32, '?');
         while (n-- > 0) {
             char* instruction = (char *) malloc(sizeof(char) * 6);
-            if (strcmp(instruction, "SET")) {
+            scanf("%[A-Z]", instruction);
+            if (strcmp(instruction, "SET") == 0) {
                 // Reading 1 number
                 int bit;
                 scanf("%d\n", &bit);
                 vec[bit] = '1';
             }
-            if (strcmp(instruction, "CLEAR")) {
+            if (strcmp(instruction, "CLEAR") == 0) {
                 // Reading 1 bit
                 int bit;
                 scanf("%d\n", &bit);
                 vec[bit] = '0';
             }
-            if (strcmp(instruction, "OR")) {
+            if (strcmp(instruction, "OR") == 0) {
                 int bit1, bit2;
                 scanf("%d%d\n", &bit1, &bit2);
                 // If a bit is 1, then OR must set bit1 to 1
@@ -31,7 +33,7 @@ int main() {
                     vec[bit1] = '?';
                 }
             }
-            if (strcmp(instruction, "AND")) {
+            if (strcmp(instruction, "AND") == 0) {
                 int bit1, bit2;
                 scanf("%d%d\n", &bit1, &bit2);
                 if (vec[bit1] == '0' || vec[bit2] == '0') {
@@ -40,6 +42,9 @@ int main() {
                     vec[bit1] = '?';
                 }
             }
-        } 
+        }
+        for (int i = 0; i < vec.size(); i++)
+            printf("%c", vec[vec.size()-i-1]);
+        printf("\n");
     }
 }
